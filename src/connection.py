eda783679fd88f31ad666ecd1ff70b9e14dd06cc
srgjo27 <96277339+srgjo27@users.noreply.github.com>
2024-05-03 11:15:06 +0700

    update

--- src/connection.py
+++ src/connection.py
@@ -1,12 +1,16 @@
 import requests
+from requests.exceptions import RequestException
 
 def get_data_from_api(endpoint):
     # Masukkan URL endpoint API Laravel Anda
-    base_url = 'http://localhost:8000/api'
+    # base_url = 'https://ud-anthony.vpnstores.net/api' # gunakan api yang diambil dari hosting
+    base_url = 'http://127.0.0.1:8000/api' # gunakan dalam local untuk melakukan test
     url = f'{base_url}/{endpoint}'
 
     # Kirim permintaan GET ke API
-    response = requests.get(url)
+    response = requests.get(url, verify=False) # aktifkan selama tahap development
+    # response = requests.get(url) # aktifkan jika sudah dalam tahap produksi
+    response.raise_for_status()
 
     # Pastikan permintaan berhasil
     if response.status_code == 200:
